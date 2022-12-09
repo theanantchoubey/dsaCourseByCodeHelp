7dceeb5ed4972e54912035be07fd64eb5b13d666
Anant Choubey <anantchoubey039@gmail.com>
2022-12-09 21:01:54 +0530

    Add Lecture010

---
+++ Lecture010-SolvingLeetCodeCodeStudioQuestions[Arrays]/lecture/problem02/sort01.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void sortOne(int arr[], int n)
+{
+    int left = 0;
+    int right = n - 1;
+    while (left < right)
+    {
+        while (arr[left] == 0 && left < right)
+        {
+            left++;
+        }
+        while (arr[right] == 1 && left < right)
+        {
+            right--;
+        }
+        //
+        if (left < right)
+        {
+
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[8] = {1, 1, 0, 0, 0, 0, 1, 0};
+    sortOne(arr, 8);
+    printArray(arr, 8);
+    return 0;
+}
