959e59bd138c1551f0ae247d61b0613ffd89fe5c
Anant Choubey <anantchoubey039@gmail.com>
2022-12-12 19:26:10 +0530

    Add Lectue024

---
+++ Lecture024-BasicMathsForDSA-Sieve-ModularArithmetics-Euclid’sAlgorithm/lecture/primeNumbers/primeNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (isPrime(n))
+    {
+        cout << "It is a Prime Number " << endl;
+    }
+    else
+    {
+        cout << "It is not a Prime Number" << endl;
+    }
+    return 0;
+}
