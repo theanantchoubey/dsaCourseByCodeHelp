e21fccfb5789fc5c6abf0c159f7cba0e63e8c0fd
Anant Choubey <anantchoubey039@gmail.com>
2022-12-13 06:04:12 +0530

    Add Lecture027

---
+++ Lecture027-DoublePointersInC++PointersPracticeMCQs/lecture/doublePointer/doublePointer.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+void update(int **p){
+    // p = p+1;
+    //Will it change? --> No
+
+    // *p = *p +1;
+    //Will it change?
+
+    // **p = **p+1;
+    //Will it change?
+}
+
+int main(){
+    // int i = 5;
+    // int *p = &i;
+    // int** p2 = &p;
+    // cout << "Working fine!"<< endl;
+    // cout << "Printing p " <<p<<endl;
+    // cout << "Printing Address of p " <<&p<<endl;
+
+    // cout << *p2 << endl;
+
+
+    //Same value for below 3 -- to print i
+    // cout << i << endl;
+    // cout << *p << endl;
+    // cout << **p2 << endl;
+
+    //Printing p
+    // cout << &i << endl;
+    // cout << p << endl;
+    // cout << *p2 << endl;
+
+    //Printing p2
+    // cout << &p << endl;
+    // cout << p2 << endl;
+
+
+    // cout  << endl << endl;
+    // cout << "Before -- > " << i <<endl;
+    // cout << "Before -- > " << p <<endl;
+    // cout << "Before -- > " << p2 <<endl;
+    // update(p2);
+    // cout << "After -- > " << i <<endl;
+    // cout << "After -- > " << p <<endl;
+    // cout << "After -- > " << p2 <<endl;
+
+    return 0;
+}
